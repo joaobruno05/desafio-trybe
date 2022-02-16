116724985c1f0b0bec07d850620d03b5d2760d28
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-16 17:42:15 -0300

    :truck: Move routes into src file

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const tasksRouter = require('./routes/tasksRoutes');
+const tasksRouter = require('./src/routes/tasksRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
